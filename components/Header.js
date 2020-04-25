586dc63ee34e709047711cf945ceeb9cd5465bbb
javieramirez90 <javier@dojodevs.io>
2020-04-25 03:40:13 -0500

    Different approach for conditional rendering with Platform API and if statements

--- components/Header.js
+++ components/Header.js
@@ -6,22 +6,35 @@ import TitleText from '../components/TitleText';
 
 const Header = (props) => {
   return(
-    <View style={styles.header}>
+    <View style={{
+      ...styles.headerBase,
+      ...Platform.select({
+        ios: styles.headeriOS,
+        android: styles.headerAndroid
+      })
+    }}>
       <TitleText style={styles.headerTitle}>{props.title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'green',
     alignItems: 'center',
-    justifyContent: 'center',
-    borderBottomColor: Platform.OS === 'ios' ? 'red' : Colors.second,
-    borderBottomWidth: Platform.OS === 'ios' ? 3 : 1
+    justifyContent: 'center'
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
+    borderBottomColor: Colors.second,
+    borderBottomWidth: 1
+  },
+  headeriOS: {
+    backgroundColor: 'green',
+    borderBottomColor: 'red',
+    borderBottomWidth: 3
   },
   headerTitle: {
     color: 'white',
