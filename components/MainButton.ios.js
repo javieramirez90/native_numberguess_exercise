5ce4ac2c622d1f96fcf6cdd4ba1a4ab80f84ed51
javieramirez90 <javier@dojodevs.io>
2020-04-25 03:59:50 -0500

    rendering different files based on os

--- components/MainButton.ios.js
+++ components/MainButton.ios.js
@@ -2,35 +2,22 @@ import React from 'react';
 import { View,
   Text,
   StyleSheet,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-  Platform
+  TouchableOpacity
 } from 'react-native';
 
 import Colors from '../constants/colors'
 
 const MainButton = props => {
-  let ButtonComponent =  TouchableOpacity;
-
-  if(Platform.OS === 'android' && Platform.Version >= 21){
-    ButtonComponent = TouchableNativeFeedback;
-  }
   return (
-    <View style={styles.buttonContainer}>
-      <ButtonComponent activeOpacity={0.5} onPress={props.onPress}>
+      <TouchableOpacity activeOpacity={0.5} onPress={props.onPress}>
         <View style={styles.button}>
         <Text style={styles.buttonText}>{props.children}</Text>
         </View>
-      </ButtonComponent>
-    </View>
+      </TouchableOpacity>
   )
 };
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    borderRadius: 30,
-    overflow: 'hidden'
-  },
   button: {
     backgroundColor: Colors.primary,
     paddingVertical: 12,
